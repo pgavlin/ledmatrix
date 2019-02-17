0356025e6db683289f18d539d3cfa5b7b5e122f8
Pat Gavlin <pgavlin@gmail.com>
2019-02-17 02:43:56 -0600

    Render LEDs using a point-spread function
    The point spread function models the lens flare and blur of the human
    visual system. The model is taken directly from [1].
    
    In an ideal world, every pixel would take every LED into account during
    rendering. However, this involves 1K PSF calculations per pixel for a
    32x32 grid of LEDs, which is not feasible on e.g. a 13" MacBook Pro.
    
    In order to achieve reasonable performance, each pixel only evaluates
    PSFs for its 49 nearest neighbors. This provides good visual fidelity at
    a fraction of the cost of the complete approach.
    
    The pixel shader is also used to render LEDs that are not turned on.
    These LEDs are represented as filled gray circles.

--- renderer.ts
+++ renderer.ts
@@ -6,75 +6,243 @@ function hex(r: number, g: number, b: number): string {
     return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
 }
 
-function redraw(framebuffer: any) {
+function createShader(gl: WebGLRenderingContext, type: number, source: string): WebGLShader {
+    const shader = gl.createShader(type)!;
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+    const log = gl.getShaderInfoLog(shader)!;
+    gl.deleteShader(shader);
+    throw new Error(log);
+}
+
+function linkProgram(gl: WebGLRenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader): WebGLProgram {
+    const program = gl.createProgram()!;
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    const log = gl.getProgramInfoLog(program)!;
+    gl.deleteProgram(program);
+    throw new Error(log);
+}
+
+function createAndLinkProgram(gl: WebGLRenderingContext, vertexSource: string, fragmentSource: string): WebGLProgram {
+    const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexSource);
+    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentSource);
+    return linkProgram(gl, vertexShader, fragmentShader);
+}
+
+function rectangle(x: number, y: number, w: number, h: number): number[] {
+    const x1 = x, x2 = x + w, y1 = y, y2 = y + h;
+    return [
+         x1, y1,
+         x2, y1,
+         x1, y2,
+         x1, y2,
+         x2, y1,
+         x2, y2,
+    ];
+}
+
+function initGl(): (leds: any) => void {
+    // Get A WebGL context
     const canvas = <HTMLCanvasElement>document.getElementById("canvas")!;
-
-    const rect = { x: 0, y: 0, w: 0, h: 0 };
-    const frameAspect = framebuffer.width / framebuffer.height;
-    const screenAspect = canvas.width / canvas.height;
-    if (frameAspect > 1.0 == screenAspect > 1.0) {
-        rect.w = canvas.width;
-        rect.h = canvas.width / frameAspect;
-        rect.y = (canvas.height - rect.h) / 2;
-    } else {
-        rect.h = canvas.height;
-        rect.w = canvas.height * frameAspect;
-        rect.x = (canvas.width - rect.w) / 2;
+    const gl = <WebGLRenderingContext>canvas.getContext("webgl");
+    if (!gl) {
+        throw new Error("WebGL is not available");
     }
 
-    const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+    gl.getExtension("GL_OES_standard_derivatives");
 
-    const pixelWidth = rect.w / framebuffer.width;
-    const pixelHeight = rect.h / framebuffer.height;
+    // shader sources
+    const vertexSource =
+`
+attribute vec4 a_position;
 
-    const outerRadius = (pixelWidth < pixelHeight ? pixelWidth : pixelHeight) / 2;
-    const innerRadius = outerRadius / 2;
+void main() {
+    gl_Position = a_position;
+}
+`;
 
-    const drawPixel = (x: number, y: number, r: number, g: number, b: number) => {
-        const cx = rect.x + x * pixelWidth + pixelWidth / 2;
-        const cy = rect.y + y * pixelHeight + pixelHeight / 2;
-
-        if (r == 0 && g == 0 && b == 0) {
-            ctx.fillStyle = hex(180, 163, 142);
-            ctx.beginPath();
-            ctx.arc(cx, cy, innerRadius * 0.8, 0, 2 * Math.PI, false);
-            ctx.fill();
-            ctx.closePath();
-        } else {
-            const rg = ctx.createRadialGradient(cx, cy, innerRadius, cx, cy, outerRadius * 0.8);
-            rg.addColorStop(0, hex(r, g, b));
-            rg.addColorStop(1, "black");
-            ctx.fillStyle = rg;
-
-            ctx.beginPath();
-            ctx.arc(cx, cy, outerRadius, 0, 2 * Math.PI, false);
-            ctx.fill();
-            ctx.closePath();
+    function genPsfs(n: number): string {
+        let s = "";
+        for (let y = 0; y < n; y++) {
+            const yc = y - Math.floor(n / 2);
+            for (let x = 0; x < n; x++) {
+                const xc = x - Math.floor(n / 2);
+                s += `\n    psf(nearestLedIndex + vec2(${xc}, ${yc}), normalized, acc);`;
+            }
         }
+        return s;
+    }
+
+    const fragmentSource =
+`
+precision mediump float;
+
+uniform sampler2D u_image;
+uniform vec2 u_resolution;
+uniform vec2 u_gridDimensions;
+
+void psf(in vec2 ledIndex, in vec2 point, inout vec4 acc) {
+    vec2 ledCenter = ledIndex / u_gridDimensions + vec2(0.5, 0.5) / u_gridDimensions;
+    vec2 texCoord = vec2(ledIndex.x, u_gridDimensions.y - ledIndex.y) / u_gridDimensions;
+    vec4 ledColor = texture2D(u_image, vec2(texCoord.x, texCoord.y));
+
+    float r = length(ledCenter - point);
+
+    float delta = 0.001;
+    float scale = 1.0 - smoothstep(0.005 - delta, 0.005 + delta, r);
+    acc += vec4(vec3(180.0, 163.0, 142.0) * scale / 255.0, 1.0);
+
+    if (ledColor.w == 0.0) {
+        return;
+    }
+
+    if (r < 0.1) {
+        float theta = atan(r) * 700.0;
+
+        float f0 = 2.61e6 * exp(-pow(theta / 0.02, 2.0));
+        float f1 = 20.91 / pow(theta + 0.02, 3.0);
+        float f2 = 72.37 / pow(theta + 0.02, 2.0);
+
+        float scale = 0.282 * f0 + 0.478 * f1 + 0.207 * f2;
+
+        acc = acc + vec4(ledColor.xyz, 1.0) * scale;
+    }
+}
+
+void main() {
+    // normalize the fragment and coordinates
+    vec2 normalized = gl_FragCoord.xy / u_resolution;
+    vec2 nearestLedIndex = floor(normalized * u_gridDimensions);
+
+    vec4 acc = vec4(0.0, 0.0, 0.0, 1.0);
+    ${genPsfs(7)}
+    gl_FragColor = acc;
+}
+`;
+
+    // setup GLSL programs
+    const program = createAndLinkProgram(gl, vertexSource, fragmentSource);
+
+    function getLocations(program: WebGLProgram) {
+        return {
+            position: gl.getAttribLocation(program, "a_position"),
+            resolution: gl.getUniformLocation(program, "u_resolution"),
+            image: gl.getUniformLocation(program, "u_image"),
+            gridDimensions: gl.getUniformLocation(program, "u_gridDimensions"),
+        };
+    }
+
+    // look up where the vertex data needs to go.
+    const locations = getLocations(program);
+
+    // Create a buffer to put three 2d clip space points in
+    const positionBuffer = gl.createBuffer();
+    // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    // Set a rectangle the same size as the image.
+    const positions = rectangle(-1, -1, 2, 2);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+//    const baseTexture = gl.createTexture();
+//    gl.bindTexture(gl.TEXTURE_2D, baseTexture);
+//
+//    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+//    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+//    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+//    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // Set up texture so we are working with pixels.
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+    let buffer: Uint8Array | undefined;
+    return (leds: any) => {
+        // Clear the OpenGL canvas.
+        gl.clearColor(0, 0, 0, 0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        // Tell it to use our program (pair of shaders)
+        gl.useProgram(program);
+
+        // Turn on the position attribute
+        gl.enableVertexAttribArray(locations.position);
+
+        // Bind the position buffer.
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+        // Tell the position attribute how to get data out of positionBuffer
+        gl.vertexAttribPointer(locations.position, 2, gl.FLOAT, false, 0, 0);
+
+        // both buffers are the same size
+        gl.uniform2f(locations.resolution, gl.canvas.width, gl.canvas.height);
+
+        // set the grid dimensions
+        gl.uniform2f(locations.gridDimensions, leds.width, leds.height);
+
+        // Create a texture for the LEDs we're drawing.
+        //
+        // TODO: add an explicit black border to avoid artifacts due to texture wrapping
+        if (buffer === undefined || buffer.length != leds.width * leds.height * 4) {
+            buffer = new Uint8Array(leds.width * leds.height * 4);
+        }
+        for (let y = 0; y < leds.height; y++) {
+            for (let x = 0; x < leds.width; x++) {
+                const lidx = x * 3 + y * leds.width * 3;
+                const r = leds.content[lidx];
+                const g = leds.content[lidx + 1];
+                const b = leds.content[lidx + 2];
+
+                const bidx = x * 4 + y * leds.width * 4;
+                buffer[bidx] = r;
+                buffer[bidx + 1] = g;
+                buffer[bidx + 2] = b;
+                buffer[bidx + 3] = r != 0 || g != 0 || b != 0 ? 1.0 : 0.0;
+            }
+        }
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, leds.width, leds.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, buffer);
+
+        // draw
+        gl.drawArrays(gl.TRIANGLES, 0, positions.length / 2);
+
+        // both buffers are the same size
+        gl.uniform2f(locations.resolution, gl.canvas.width, gl.canvas.height);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        gl.drawArrays(gl.TRIANGLES, 0, positions.length / 2);
     };
-
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    for (let y = 0; y < framebuffer.height; y++) {
-        for (let x = 0; x < framebuffer.width; x++) {
-            const i = x * 3 + y * framebuffer.width * 3;
-            drawPixel(x, y, framebuffer.content[i], framebuffer.content[i+1], framebuffer.content[i+2]);
-        }
-    }
 }
 
-async function scheduleRedraw() {
-    const resp = await (await window.fetch("/framebuffer")).json();
-    resp.content = base64_decode(resp.content);
+let last = undefined;
+async function scheduleRedraw(render: (leds: any) => void) {
+    const t = Date.now();
+    const leds = await (await window.fetch("/framebuffer")).json();
+    leds.content = base64_decode(leds.content);
     window.requestAnimationFrame(() => {
-        redraw(resp);
-        scheduleRedraw();
+        render(leds);
+        scheduleRedraw(render);
     });
+    last = t;
 }
 
-window.onload = () => {
+window.onload = async () => {
     const canvas = <HTMLCanvasElement>document.getElementById("canvas");
-    canvas.width = document.body.clientWidth;
+    canvas.width = document.body.clientHeight;
     canvas.height = document.body.clientHeight;
-    scheduleRedraw();
+    scheduleRedraw(initGl());
 };
